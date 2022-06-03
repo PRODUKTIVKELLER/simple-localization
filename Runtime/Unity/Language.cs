5cb13b576dbe4ff3030602a15a41c8d44d9bb5f9
SimonBlasen <simon.yzf@t-online.de>
2022-06-03 14:30:20 +0200

    [FEATURE] Add OnlyActiveForLanguage class for selectevely activating gameobjects depending on language.

--- Runtime/Unity/Language.cs
+++ Runtime/Unity/Language.cs
@@ -3,6 +3,7 @@
     public enum Language
     {
         EN,
-        DE
+        DE,
+        CHN
     }
 }
---
+++ Runtime/Unity/OnlyActiveForLanguage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Produktivkeller.SimpleLocalization.Unity
+{
+    public class OnlyActiveForLanguage : MonoBehaviour, ILocalized
+    {
+        public Language language;
+
+        private void Start()
+        {
+            RefreshActiveState();
+        }
+
+        private void OnEnable()
+        {
+            RefreshActiveState();
+        }
+
+        public void OnLanguageHasChanged()
+        {
+            RefreshActiveState();
+        }
+
+        private void RefreshActiveState()
+        {
+            LocalizationService localizationService = LocalizationService.Instance;
+
+            // Check is required because "OnEnable" can be called before LanguageService is initialized.
+            if (localizationService)
+            {
+                gameObject.SetActive(localizationService.CurrentLanguage == language);
+            }
+        }
+    }
+}
