78feb0f75fbc06030b3135b060b9aba938cddd27
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-06-09 21:00:14 +0200

    [FEATURE] Add 'LocalizedFont' to only change font without requesting a localized string.

---
+++ Runtime/Unity/Components/LocalizedFont.cs
@@ -0,0 +1,43 @@
+﻿using Produktivkeller.SimpleLocalization.Unity.Core;
+using TMPro;
+using UnityEngine;
+
+namespace Produktivkeller.SimpleLocalization.Unity.Components
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedFont : MonoBehaviour, ILocalized
+    {
+        private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            ResolveLocalization();
+        }
+
+        private void OnEnable()
+        {
+            ResolveLocalization();
+        }
+
+        public void OnLanguageHasChanged()
+        {
+            ResolveLocalization();
+        }
+
+        private void ResolveLocalization()
+        {
+            if (_text == null)
+            {
+                _text = GetComponent<TextMeshProUGUI>();
+            }
+
+            // Check is required because "OnEnable" can be called before LanguageService is initialized.
+            if (!LocalizationService.Instance)
+            {
+                return;
+            }
+
+            LocalizationService.Instance.UpdateFont(gameObject);
+        }
+    }
+}
