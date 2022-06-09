89cef8711bdc2b489e3879fefa8bf17ecd6bf50d
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-06-09 12:30:08 +0200

    [FEATURE] Add 'ScriptableObject' for languages to configure them per application.

---
+++ Runtime/Unity/Data/Language.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Produktivkeller.SimpleLocalization.Unity.Data
+{
+    [CreateAssetMenu(fileName = "Language", menuName = "PRODUKTIVKELLER/Simple Localization/Language")]
+    public class Language : ScriptableObject
+    {
+        public SystemLanguage systemLanguage;
+        public bool           isDefaultLanguage;
+        
+        public override string ToString()
+        {
+            return systemLanguage.ToString();
+        }
+    }
+}
