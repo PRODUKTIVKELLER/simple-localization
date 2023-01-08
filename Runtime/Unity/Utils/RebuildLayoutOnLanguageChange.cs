77c52231ee620c23527633b7ba45e209c3b3f78e
Stefan Bechert <stefan.bechert.inf@gmail.com>
2023-01-08 12:17:47 +0100

    [VERSION/FEATURE] Increase version to '0.5.4' and add script for rebuilding the layout after the language has changed.

---
+++ Runtime/Unity/Utils/RebuildLayoutOnLanguageChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Produktivkeller.SimpleLocalization.Unity.Components;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Produktivkeller.SimpleLocalization.Unity.Utils
+{
+    public class RebuildLayoutOnLanguageChange : MonoBehaviour, ILocalized
+    {
+        public void OnLanguageHasChanged()
+        {
+            StartCoroutine(RebuildLayout());
+        }
+
+        private IEnumerator RebuildLayout()
+        {
+            yield return null;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+        }
+    }
+}
